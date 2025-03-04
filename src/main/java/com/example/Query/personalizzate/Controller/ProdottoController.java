d0a5938d12b3f450e381562cf12b345231ce4ea9
Chibinini <pirainocostanza@gmail.com>
2025-03-04 12:22:51 +0100

    Modificato application.yml implementato get e post su controller

--- src/main/java/com/example/Query/personalizzate/Controller/ProdottoController.java
+++ src/main/java/com/example/Query/personalizzate/Controller/ProdottoController.java
@@ -4,10 +4,9 @@ import com.example.Query.personalizzate.Entity.ProdottoEntity;
 import com.example.Query.personalizzate.Service.ProdottoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/prodotto")
@@ -15,10 +14,15 @@ public class ProdottoController {
     @Autowired
     ProdottoService prodottoService;
 
-    @PostMapping
+    @PostMapping("/crea")
     public ResponseEntity<ProdottoEntity> creaProdotto(@RequestBody ProdottoEntity prodotto){
     ProdottoEntity prodotto1 = prodottoService.creaProdotto(prodotto);
     return ResponseEntity.ok(prodotto1);
+    }
+
+    @GetMapping("/lista")
+    public ResponseEntity<List<ProdottoEntity>> ritornaLista(){
+        return ResponseEntity.ok(prodottoService.readAllProduct());
     }
 
 }
